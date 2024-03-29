1b70eda988543616f8896c5013e5a2d87847e83f
aoife cassidy <aoife@cassidy.sh>
2024-03-30 02:05:02 +0300

    Add CLI boilerplate

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { VAD, VADEventType, VADStream } from './vad';
-import { Plugin } from './plugin';
-
-const version = '0.1.0';
-
-export {
-  version,
-  VAD,
-  VADEventType,
-  VADStream,
-  Plugin,
-};
+export { VAD, VADEventType, VADStream } from './vad';
+export { Plugin } from './plugin';
+export { version } from './version';
---
+++ src/version.ts
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+export const version = '0.1.0';
