6c793a44ccd4bd0530a170eec1b083e35959e34c
aoife cassidy <aoife@cassidy.sh>
2024-04-01 17:02:18 +0300

    Add STT and utils

---
+++ agents/src/stt/stt.ts
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { AudioFrame } from '@livekit/rtc-node';
+import { AudioBuffer } from '../utils';
+
+export enum SpeechEventType {
+  START_OF_SPEECH = 0,
+  INTERIM_TRANSCRIPT = 1,
+  FINAL_TRANSCRIPT = 2,
+  END_OF_SPEECH = 3,
+}
+
+export interface SpeechData {
+  language: string;
+  text: string;
+  startTime: number;
+  endTime: number;
+  confidence: number;
+}
+
+export class SpeechEvent {
+  type: SpeechEventType;
+  alternatives: SpeechData[];
+
+  constructor(type: SpeechEventType, alternatives: SpeechData[] = []) {
+    this.type = type;
+    this.alternatives = alternatives;
+  }
+}
+
+export abstract class SpeechStream implements IterableIterator<SpeechEvent> {
+  abstract pushFrame(token: AudioFrame): void;
+
+  abstract close(wait: boolean): Promise<void>;
+
+  abstract next(): IteratorResult<SpeechEvent>;
+
+  [Symbol.iterator](): SpeechStream {
+    return this;
+  }
+}
+
+export abstract class STT {
+  #streamingSupported: boolean;
+
+  constructor(streamingSupported: boolean) {
+    this.#streamingSupported = streamingSupported;
+  }
+
+  abstract recognize(buffer: AudioBuffer, language: string | undefined): Promise<SpeechEvent>;
+
+  abstract stream(language: string | undefined): SpeechStream;
+
+  get streamingSupported(): boolean {
+    return this.#streamingSupported;
+  }
+}
---
+++ agents/src/utils.ts
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { AudioFrame } from '@livekit/rtc-node';
+
+export type AudioBuffer = AudioFrame[] | AudioFrame;
+
+export const mergeFrames = (buffer: AudioBuffer): AudioFrame => {
+  if (Array.isArray(buffer)) {
+    buffer = buffer as AudioFrame[];
+    if (buffer.length == 0) {
+      throw new TypeError('buffer is empty');
+    }
+
+    const sampleRate = buffer[0].sampleRate;
+    const channels = buffer[0].channels;
+    let samplesPerChannel = 0;
+    let data = new Uint16Array();
+
+    for (const frame of buffer) {
+      if (frame.sampleRate !== sampleRate) {
+        throw new TypeError('sample rate mismatch');
+      }
+
+      if (frame.channels !== channels) {
+        throw new TypeError('channel count mismatch');
+      }
+
+      data = new Uint16Array([...data, ...frame.data]);
+      samplesPerChannel += frame.samplesPerChannel;
+    }
+
+    return new AudioFrame(data, sampleRate, channels, samplesPerChannel);
+  }
+
+  return buffer;
+};
