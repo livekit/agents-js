8070085048958d4db146d716b53b5c1d27b3b1e0
aoife cassidy <aoife@cassidy.sh>
2024-04-06 00:19:12 +0300

    Add HTTP server impl

---
+++ agents/src/http_server.ts
@@ -0,0 +1,44 @@
+import { createServer, ServerResponse, Server, IncomingMessage } from 'http';
+
+const healthCheck = async (res: ServerResponse) => {
+  res.writeHead(200);
+  res.end('OK');
+};
+
+export class HTTPServer {
+  host: string;
+  port: number;
+  app: Server;
+
+  constructor(host: string, port: number) {
+    this.host = host;
+    this.port = port;
+
+    this.app = createServer((req: IncomingMessage, res: ServerResponse) => {
+      if (req.url === '/') {
+        healthCheck(res);
+      } else {
+        res.writeHead(404);
+        res.end('not found');
+      }
+    });
+  }
+
+  async run(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.app.listen(this.port, this.host, (err?: Error) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  }
+
+  async close(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.app.close((err?: Error) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  }
+}
