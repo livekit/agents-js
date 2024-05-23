c14f2edda3bd6f20943ec7652dc59a2fc2e916f3
lukasIO <mail@lukasseiler.de>
2024-05-23 12:24:20 +0200

    Let defineAgent accept agent type (#13)

--- examples/src/minimal.ts
+++ examples/src/minimal.ts
@@ -1,7 +1,14 @@
 // SPDX-FileCopyrightText: 2024 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
-import { type JobContext, type JobRequest, WorkerOptions, cli, defineAgent } from '@livekit/agents';
+import {
+  type Agent,
+  type JobContext,
+  type JobRequest,
+  WorkerOptions,
+  cli,
+  defineAgent,
+} from '@livekit/agents';
 import { fileURLToPath } from 'url';
 
 const requestFunc = async (req: JobRequest) => {
@@ -13,10 +20,13 @@ if (process.argv[1] === fileURLToPath(import.meta.url)) {
   cli.runApp(new WorkerOptions({ requestFunc }));
 }
 
+const myAgent: Agent = {
+  entry: async (job: JobContext) => {
+    console.log('starting voice assistant...');
+    job;
+  },
+};
+
 // your entry file has to provide a default export of type Agent.
 // use the defineAgent() helper function to generate your agent.
-export default defineAgent(async (job: JobContext) => {
-  console.log('starting voice assistant...');
-  job;
-  // etc
-});
+export default defineAgent(myAgent);
