4c374354166810b2dc6944caa72e8b3d4a097f0e
Ben Cherry <ben@livekit.io>
2024-09-04 15:52:39 -0700

    Update deps

--- examples/src/minimal.ts
+++ examples/src/minimal.ts
@@ -1,32 +1,51 @@
 // SPDX-FileCopyrightText: 2024 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+// index.ts
+
 import {
-  type Agent,
   type JobContext,
   type JobRequest,
   WorkerOptions,
   cli,
-  defineAgent,
+  defineAgent
 } from '@livekit/agents';
-import { fileURLToPath } from 'url';
+import { TTS } from '@livekit/agents-plugin-elevenlabs';
+import { AudioSource, LocalAudioTrack, TrackPublishOptions, TrackSource } from '@livekit/rtc-node';
 
+export default defineAgent({
+  entry: async (job: JobContext) => {
+    console.log('starting TTS example agent')
+
+    // prepare our audio track and start publishing it to the room
+    const source = new AudioSource(24000, 1);
+    const track = LocalAudioTrack.createAudioTrack('agent-mic', source);
+    const options = new TrackPublishOptions();
+    options.source = TrackSource.SOURCE_MICROPHONE;
+    await job.room.localParticipant?.publishTrack(track, options);
+
+    // ask ElevenLabs to synthesize "Hello!"
+    const tts = new TTS();
+    console.log('speaking "Hello!"');
+    await tts
+      .synthesize('Hello!')
+      .then((output) => output.collect())
+      .then((output) => {
+        // send the audio to our track
+        source.captureFrame(output);
+      });
+  }
+})
+
+// the requestFunc function allows us to do some things on the main thread after worker connection
 const requestFunc = async (req: JobRequest) => {
-  console.log('received request', req);
-  await req.accept(fileURLToPath(import.meta.url));
-};
-
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  cli.runApp(new WorkerOptions({ requestFunc }));
+  // this line needs to be exact.
+  // we are passing this file's path to Agents, in order to import it later and run our entry function.
+  await req.accept(import.meta.filename)
 }
 
-const myAgent: Agent = {
-  entry: async (job: JobContext) => {
-    console.log('starting voice assistant...');
-    job;
-  },
-};
-
-// your entry file has to provide a default export of type Agent.
-// use the defineAgent() helper function to generate your agent.
-export default defineAgent(myAgent);
+// check that we're running this file and not importing functions from it
+// without this if closure, our code would start` a new Agents process on every job process.
+if (process.argv[1] === import.meta.filename) {
+  cli.runApp(new WorkerOptions({ requestFunc }));
+}
