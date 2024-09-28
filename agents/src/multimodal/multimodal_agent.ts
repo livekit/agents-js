d641b4db162b58efb2d24855786c7e76773a11fd
Ben Cherry <ben@livekit.io>
2024-09-27 18:01:26 -0700

    lint

--- agents/src/multimodal/multimodal_agent.ts
+++ agents/src/multimodal/multimodal_agent.ts
@@ -90,7 +90,10 @@ export class MultimodalAgent {
     }
   }
 
-  start(room: Room, participant: RemoteParticipant | string | null = null): Promise<RealtimeSession> {
+  start(
+    room: Room,
+    participant: RemoteParticipant | string | null = null,
+  ): Promise<RealtimeSession> {
     return new Promise(async (resolve, reject) => {
       if (this.#started) {
         this.#logger.warn('MultimodalAgent already started');
