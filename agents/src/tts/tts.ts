80f6db46914a98eab7c75748fe8f091aa8681ad4
aoife cassidy <aoife@livekit.io>
2024-10-08 21:02:27 +0300

    feat(stt,tts): update baseclasses to match python (#103)

--- agents/src/tts/tts.ts
+++ agents/src/tts/tts.ts
@@ -2,87 +2,135 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 import type { AudioFrame } from '@livekit/rtc-node';
-import { mergeFrames } from '../utils.js';
+import { AsyncIterableQueue } from '../utils.js';
 
+/** SynthesizedAudio is a packet of speech synthesis as returned by the TTS. */
 export interface SynthesizedAudio {
-  text: string;
-  data: AudioFrame;
+  /** Request ID (one segment could be made up of multiple requests) */
+  requestId: string;
+  /** Segment ID, each segment is separated by a flush */
+  segmentId: string;
+  /** Synthesized audio frame */
+  frame: AudioFrame;
+  /** Current segment of the synthesized audio */
+  deltaText: string;
 }
 
-export enum SynthesisEventType {
-  /**
-   * Indicate the start of synthesis.
-   * Retriggered after FINISHED.
-   */
-  STARTED = 0,
-  /**
-   * Indicate that audio data is available.
-   */
-  AUDIO = 1,
-  /**
-   * Indicate the end of synthesis. Does not necessarily mean stream is done.
-   */
-  FINISHED = 2,
+/**
+ * Describes the capabilities of the TTS provider.
+ *
+ * @remarks
+ * At present, only `streaming` is supplied to this interface, and the framework only supports
+ * providers that do have a streaming endpoint.
+ */
+export interface TTSCapabilities {
+  streaming: boolean;
 }
 
-export class SynthesisEvent {
-  type: SynthesisEventType;
-  audio?: SynthesizedAudio;
-
-  constructor(type: SynthesisEventType, audio: SynthesizedAudio | undefined = undefined) {
-    this.type = type;
-    this.audio = audio;
-  }
-}
-
-export abstract class SynthesizeStream implements IterableIterator<SynthesisEvent> {
-  abstract pushText(token?: string): void;
-
-  markSegmentEnd() {
-    this.pushText(undefined);
-  }
-
-  abstract close(wait: boolean): Promise<void>;
-  abstract next(): IteratorResult<SynthesisEvent>;
-
-  [Symbol.iterator](): SynthesizeStream {
-    return this;
-  }
-}
-
+/**
+ * An instance of a text-to-speech adapter.
+ *
+ * @remarks
+ * This class is abstract, and as such cannot be used directly. Instead, use a provider plugin that
+ * exports its own child TTS class, which inherits this class's methods.
+ */
 export abstract class TTS {
-  #streamingSupported: boolean;
+  #capabilities: TTSCapabilities;
+  #sampleRate: number;
+  #numChannels: number;
 
-  constructor(streamingSupported: boolean) {
-    this.#streamingSupported = streamingSupported;
+  constructor(sampleRate: number, numChannels: number, capabilities: TTSCapabilities) {
+    this.#capabilities = capabilities;
+    this.#sampleRate = sampleRate;
+    this.#numChannels = numChannels;
   }
 
-  abstract synthesize(text: string): Promise<ChunkedStream>;
+  /** Returns this TTS's capabilities */
+  get capabilities(): TTSCapabilities {
+    return this.#capabilities;
+  }
 
+  /** Returns the sample rate of audio frames returned by this TTS */
+  get sampleRate(): number {
+    return this.#sampleRate;
+  }
+
+  /** Returns the channel count of audio frames returned by this TTS */
+  get numChannels(): number {
+    return this.#numChannels;
+  }
+
+  /**
+   * Returns a {@link SynthesizeStream} that can be used to push text and receive audio data
+   */
   abstract stream(): SynthesizeStream;
-
-  get streamingSupported(): boolean {
-    return this.#streamingSupported;
-  }
 }
 
-export abstract class ChunkedStream implements AsyncIterableIterator<SynthesizedAudio> {
-  async collect(): Promise<AudioFrame> {
-    const frames = [];
-    for await (const ev of this) {
-      frames.push(ev.data);
+/**
+ * An instance of a text-to-speech stream, as an asynchronous iterable iterator.
+ *
+ * @example Looping through frames
+ * ```ts
+ * for await (const event of stream) {
+ *   await source.captureFrame(event.frame);
+ * }
+ * ```
+ *
+ * @remarks
+ * This class is abstract, and as such cannot be used directly. Instead, use a provider plugin that
+ * exports its own child SynthesizeStream class, which inherits this class's methods.
+ */
+export abstract class SynthesizeStream implements AsyncIterableIterator<SynthesizedAudio> {
+  protected static readonly FLUSH_SENTINEL = Symbol('FLUSH_SENTINEL');
+  protected input = new AsyncIterableQueue<string | typeof SynthesizeStream.FLUSH_SENTINEL>();
+  protected queue = new AsyncIterableQueue<SynthesizedAudio>();
+  protected closed = false;
+
+  /** Push a string of text to the TTS */
+  pushText(text: string) {
+    if (this.input.closed) {
+      throw new Error('Input is closed');
     }
-    return mergeFrames(frames);
+    if (this.closed) {
+      throw new Error('Stream is closed');
+    }
+    this.input.put(text);
   }
 
-  abstract close(): Promise<void>;
-  abstract next(): Promise<IteratorResult<SynthesizedAudio>>;
-
-  [Symbol.iterator](): ChunkedStream {
-    return this;
+  /** Flush the TTS, causing it to process all pending text */
+  flush() {
+    if (this.input.closed) {
+      throw new Error('Input is closed');
+    }
+    if (this.closed) {
+      throw new Error('Stream is closed');
+    }
+    this.input.put(SynthesizeStream.FLUSH_SENTINEL);
   }
 
-  [Symbol.asyncIterator](): ChunkedStream {
+  /** Mark the input as ended and forbid additional pushes */
+  endInput() {
+    if (this.input.closed) {
+      throw new Error('Input is closed');
+    }
+    if (this.closed) {
+      throw new Error('Stream is closed');
+    }
+    this.input.close();
+  }
+
+  next(): Promise<IteratorResult<SynthesizedAudio>> {
+    return this.queue.next();
+  }
+
+  /** Close both the input and output of the TTS stream */
+  close() {
+    this.input.close();
+    this.queue.close();
+    this.closed = true;
+  }
+
+  [Symbol.asyncIterator](): SynthesizeStream {
     return this;
   }
 }
