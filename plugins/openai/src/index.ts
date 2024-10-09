d686f6dfddf368b5cdb273794873dc645d7cc63a
lukasIO <mail@lukasseiler.de>
2024-10-09 12:17:34 +0200

    Use API extractor

--- plugins/openai/src/index.ts
+++ plugins/openai/src/index.ts
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: 2024 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
-export * as realtime from './realtime/index.js';
+import * as realtime from './realtime/index.js';
+
+export { realtime };
