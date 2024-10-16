5b7076d4391374312f4746aa0714165aeec27e2c
aoife cassidy <aoife@livekit.io>
2024-10-17 00:28:53 +0300

    only support pcm for now

--- plugins/elevenlabs/src/models.ts
+++ plugins/elevenlabs/src/models.ts
@@ -10,12 +10,11 @@ export type TTSModels =
   | 'eleven_turbo_v2_5';
 
 export type TTSEncoding =
-  | 'mp3_22050_32'
-  | 'mp3_44100_32'
-  | 'mp3_44100_64'
-  | 'mp3_44100_96'
-  | 'mp3_44100_128'
-  | 'mp3_44100_192'
-  | 'pcm_16000'
-  | 'pcm_22050'
-  | 'pcm_44100';
+  // XXX(nbsp): MP3 is not yet supported
+  // | 'mp3_22050_32'
+  // | 'mp3_44100_32'
+  // | 'mp3_44100_64'
+  // | 'mp3_44100_96'
+  // | 'mp3_44100_128'
+  // | 'mp3_44100_192'
+  'pcm_16000' | 'pcm_22050' | 'pcm_44100';
