b0f7ff6702a43a322970d2a1842aa254833b2a93
aoife cassidy <aoife@livekit.io>
2024-10-26 20:11:50 +0300

    feat(deepgram): init Deepgram STT (#115)

---
+++ .changeset/friendly-wombats-help.md
@@ -0,0 +1,6 @@
+---
+"@livekit/agents": patch
+"@livekit/agents-plugin-deepgram": minor
+---
+
+add Deepgram text-to-speech plugin
--- agents/src/utils.ts
+++ agents/src/utils.ts
@@ -262,6 +262,7 @@ export class AsyncIterableQueue<T> implements AsyncIterableIterator<T> {
   }
 }
 
+/** @internal */
 export class ExpFilter {
   #alpha: number;
   #max?: number;
@@ -300,5 +301,33 @@ export class ExpFilter {
 
   set alpha(alpha: number) {
     this.#alpha = alpha;
+  }
+}
+
+/** @internal */
+export class AudioEnergyFilter {
+  #cooldownSeconds: number;
+  #cooldown: number;
+
+  constructor(cooldownSeconds = 1) {
+    this.#cooldownSeconds = cooldownSeconds;
+    this.#cooldown = cooldownSeconds;
+  }
+
+  pushFrame(frame: AudioFrame): boolean {
+    const arr = Float32Array.from(frame.data, (x) => x / 32768);
+    const rms = (arr.map((x) => x ** 2).reduce((acc, x) => acc + x) / arr.length) ** 0.5;
+    if (rms > 0.004) {
+      this.#cooldown = this.#cooldownSeconds;
+      return true;
+    }
+
+    const durationSeconds = frame.samplesPerChannel / frame.sampleRate;
+    this.#cooldown -= durationSeconds;
+    if (this.#cooldown > 0) {
+      return true;
+    }
+
+    return false;
   }
 }
