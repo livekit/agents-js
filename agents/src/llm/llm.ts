bc7e9f8ef9d5842c1a2e3357568519800c3b2c6f
aoife cassidy <aoife@livekit.io>
2024-11-02 07:54:24 +0200

    feat(llm): add LLM, LLMStream
    deferred function calling depended on adding an additional type to
    function_context.ts. i called it `DeferredFunction`, but maybe
    @/theomonnom has a better name for it?

---
+++ agents/src/llm/llm.ts
@@ -0,0 +1,106 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+import { AsyncIterableQueue } from '../utils.js';
+import type { ChatContext, ChatRole } from './chat_context.js';
+import type {
+  CallableFunctionResult,
+  DeferredFunction,
+  FunctionContext,
+} from './function_context.js';
+
+export interface ChoiceDelta {
+  role: ChatRole;
+  content?: string;
+  toolCalls?: FunctionContext;
+}
+
+export interface CompletionUsage {
+  completionTokens: number;
+  promptTokens: number;
+  totalTokens: number;
+}
+
+export interface Choice {
+  delta: ChoiceDelta;
+  index: number;
+}
+
+export interface ChatChunk {
+  requestId: string;
+  choices: Choice[];
+  usage?: CompletionUsage;
+}
+
+export abstract class LLM {
+  /**
+   * Returns a {@link LLMStream} that can be used to push text and receive LLM responses.
+   */
+  abstract chat({
+    chatCtx,
+    fncCtx,
+    temperature,
+    n,
+    parallelToolCalls,
+  }: {
+    chatCtx: ChatContext;
+    fncCtx?: FunctionContext;
+    temperature?: number;
+    n?: number;
+    parallelToolCalls?: boolean;
+  }): LLMStream;
+}
+
+export abstract class LLMStream implements AsyncIterableIterator<ChatChunk> {
+  protected queue = new AsyncIterableQueue<ChatChunk>();
+  protected closed = false;
+
+  #chatCtx: ChatContext;
+  #fncCtx?: FunctionContext;
+  #functionCalls: DeferredFunction[] = [];
+
+  constructor(chatCtx: ChatContext, fncCtx?: FunctionContext) {
+    this.#chatCtx = chatCtx;
+    this.#fncCtx = fncCtx;
+  }
+
+  /** List of called functions from this stream. */
+  get functionCalls(): DeferredFunction[] {
+    return this.#functionCalls;
+  }
+
+  /** The function context of this stream. */
+  get fncCtx(): FunctionContext | undefined {
+    return this.#fncCtx;
+  }
+
+  /** The initial chat context of this stream. */
+  get chatCtx(): ChatContext {
+    return this.#chatCtx;
+  }
+
+  /** Execute all deferred functions of this stream concurrently. */
+  async executeFunctions(): Promise<CallableFunctionResult[]> {
+    return Promise.all(
+      this.#functionCalls.map((f) =>
+        f.func.execute(f.params).then(
+          (result) => ({ name: f.name, toolCallId: f.toolCallId, result }),
+          (error) => ({ name: f.name, toolCallId: f.toolCallId, error }),
+        ),
+      ),
+    );
+  }
+
+  next(): Promise<IteratorResult<ChatChunk>> {
+    return this.queue.next();
+  }
+
+  close() {
+    this.queue.close();
+    this.closed = true;
+  }
+
+  [Symbol.asyncIterator](): LLMStream {
+    return this;
+  }
+}
