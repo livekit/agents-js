c1b00f1040a42b87ad5e1b9769a24525919e84b7
aoife cassidy <aoife@livekit.io>
2024-11-03 13:15:51 +0200

    add adapters

--- plugins/openai/src/llm/llm.ts
+++ plugins/openai/src/llm/llm.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 import { llm, log } from '@livekit/agents';
-import { OpenAI } from 'openai';
+import { AzureOpenAI, OpenAI } from 'openai';
 import type {
   CerebrasChatModels,
   ChatModels,
@@ -21,6 +21,7 @@ export interface LLMOptions {
   baseURL?: string;
   user?: string;
   temperature?: number;
+  client?: OpenAI;
 }
 
 const defaultLLMOptions: LLMOptions = {
@@ -28,26 +29,356 @@ const defaultLLMOptions: LLMOptions = {
   apiKey: process.env.OPENAI_API_KEY,
 };
 
+const defaultAzureLLMOptions: LLMOptions = {
+  model: 'gpt-4o',
+  apiKey: process.env.AZURE_API_KEY,
+};
+
 export class LLM extends llm.LLM {
   #opts: LLMOptions;
   #client: OpenAI;
 
+  /**
+   * Create a new instance of OpenAI LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your OpenAI API key, either using the argument or by setting the
+   * `OPENAI_API_KEY` environmental variable.
+   */
   constructor(opts: Partial<LLMOptions> = defaultLLMOptions) {
     super();
 
     this.#opts = { ...defaultLLMOptions, ...opts };
-
     if (this.#opts.apiKey === undefined) {
       throw new Error('OpenAI API key is required, whether as an argument or as $OPENAI_API_KEY');
     }
 
-    this.#client = new OpenAI({
-      baseURL: opts.baseURL,
-      apiKey: opts.apiKey,
+    this.#client =
+      this.#opts.client ||
+      new OpenAI({
+        baseURL: opts.baseURL,
+        apiKey: opts.apiKey,
+      });
+  }
+
+  /**
+   * Create a new instance of OpenAI LLM with Azure.
+   *
+   * @remarks
+   * This automatically infers the following arguments from their corresponding environment variables if they are not provided:
+   * - `apiKey` from `AZURE_OPENAI_API_KEY`
+   * - `organization` from `OPENAI_ORG_ID`
+   * - `project` from `OPENAI_PROJECT_ID`
+   * - `azureAdToken` from `AZURE_OPENAI_AD_TOKEN`
+   * - `apiVersion` from `OPENAI_API_VERSION`
+   * - `azureEndpoint` from `AZURE_OPENAI_ENDPOINT`
+   */
+  static withAzure(
+    opts: {
+      model: string | ChatModels;
+      azureEndpoint?: string;
+      azureDeployment?: string;
+      apiVersion?: string;
+      apiKey?: string;
+      azureAdToken?: string;
+      azureAdTokenProvider?: () => Promise<string>;
+      organization?: string;
+      project?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+    } = defaultAzureLLMOptions,
+  ): LLM {
+    opts = { ...defaultLLMOptions, ...opts };
+    if (opts.apiKey === undefined) {
+      throw new Error('Azure API key is required, whether as an argument or as $AZURE_API_KEY');
+    }
+
+    return new LLM({
+      temperature: opts.temperature,
+      user: opts.user,
+      client: new AzureOpenAI(opts),
     });
   }
 
-  // TODO(nbsp): with* adapters
+  /**
+   * Create a new instance of Cerebras LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your Cerebras API key, either using the argument or by setting the
+   * `CEREBRAS_API_KEY` environmental variable.
+   */
+  static withCerebras(
+    opts: Partial<{
+      model: string | CerebrasChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.CEREBRAS_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error(
+        'Cerebras API key is required, whether as an argument or as $CEREBRAS_API_KEY',
+      );
+    }
+
+    return new LLM({
+      model: 'llama3.1-8b',
+      baseURL: 'https://api.cerebras.ai/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of Fireworks LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your Fireworks API key, either using the argument or by setting the
+   * `FIREWORKS_API_KEY` environmental variable.
+   */
+  static withFireworks(opts: Partial<LLMOptions> = {}): LLM {
+    opts.apiKey = opts.apiKey || process.env.FIREWORKS_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error(
+        'Fireworks API key is required, whether as an argument or as $FIREWORKS_API_KEY',
+      );
+    }
+
+    return new LLM({
+      model: 'accounts/fireworks/models/llama-v3p1-70b-instruct',
+      baseURL: 'https://api.fireworks.ai/inference/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of xAI LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your xAI API key, either using the argument or by setting the
+   * `XAI_API_KEY` environmental variable.
+   */
+  static withXAI(
+    opts: Partial<{
+      model: string | XAIChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.XAI_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error('xAI API key is required, whether as an argument or as $XAI_API_KEY');
+    }
+
+    return new LLM({
+      model: 'grok-2-public',
+      baseURL: 'https://api.x.ai/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of Groq LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your Groq API key, either using the argument or by setting the
+   * `GROQ_API_KEY` environmental variable.
+   */
+  static withGroq(
+    opts: Partial<{
+      model: string | GroqChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.GROQ_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error('Groq API key is required, whether as an argument or as $GROQ_API_KEY');
+    }
+
+    return new LLM({
+      model: 'llama3-8b-8192',
+      baseURL: 'https://api.groq.com/openai/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of DeepSeek LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your DeepSeek API key, either using the argument or by setting the
+   * `DEEPSEEK_API_KEY` environmental variable.
+   */
+  static withDeepSeek(
+    opts: Partial<{
+      model: string | DeepSeekChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.DEEPSEEK_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error(
+        'DeepSeek API key is required, whether as an argument or as $DEEPSEEK_API_KEY',
+      );
+    }
+
+    return new LLM({
+      model: 'deepseek-chat',
+      baseURL: 'https://api.deepseek.com/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of OctoAI LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your OctoAI API key, either using the argument or by setting the
+   * `OCTOAI_TOKEN` environmental variable.
+   */
+  static withOcto(
+    opts: Partial<{
+      model: string | OctoChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.OCTOAI_TOKEN;
+    if (opts.apiKey === undefined) {
+      throw new Error('OctoAI API key is required, whether as an argument or as $OCTOAI_TOKEN');
+    }
+
+    return new LLM({
+      model: 'llama-2-13b-chat',
+      baseURL: 'https://text.octoai.run/v1',
+      ...opts,
+    });
+  }
+
+  /** Create a new instance of Ollama LLM. */
+  static withOllama(
+    opts: Partial<{
+      model: string;
+      baseURL?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    return new LLM({
+      model: 'llama-2-13b-chat',
+      baseURL: 'https://text.octoai.run/v1',
+      apiKey: 'ollama',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of PerplexityAI LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your PerplexityAI API key, either using the argument or by setting the
+   * `PERPLEXITY_API_KEY` environmental variable.
+   */
+  static withPerplexity(
+    opts: Partial<{
+      model: string | PerplexityChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.PERPLEXITY_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error(
+        'PerplexityAI API key is required, whether as an argument or as $PERPLEXITY_API_KEY',
+      );
+    }
+
+    return new LLM({
+      model: 'llama-3.1-sonar-small-128k-chat',
+      baseURL: 'https://api.perplexity.ai',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of TogetherAI LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your TogetherAI API key, either using the argument or by setting the
+   * `TOGETHER_API_KEY` environmental variable.
+   */
+  static withTogether(
+    opts: Partial<{
+      model: string | TogetherChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.TOGETHER_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error(
+        'TogetherAI API key is required, whether as an argument or as $TOGETHER_API_KEY',
+      );
+    }
+
+    return new LLM({
+      model: 'meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo',
+      baseURL: 'https://api.together.xyz/v1',
+      ...opts,
+    });
+  }
+
+  /**
+   * Create a new instance of Telnyx LLM.
+   *
+   * @remarks
+   * `apiKey` must be set to your Telnyx API key, either using the argument or by setting the
+   * `TELNYX_API_KEY` environmental variable.
+   */
+  static withTelnyx(
+    opts: Partial<{
+      model: string | TelnyxChatModels;
+      apiKey?: string;
+      baseURL?: string;
+      user?: string;
+      temperature?: number;
+      client: OpenAI;
+    }> = {},
+  ): LLM {
+    opts.apiKey = opts.apiKey || process.env.TELNYX_API_KEY;
+    if (opts.apiKey === undefined) {
+      throw new Error('Telnyx API key is required, whether as an argument or as $TELNYX_API_KEY');
+    }
+
+    return new LLM({
+      model: 'meta-llama/Meta-Llama-3.1-70B-Instruct',
+      baseURL: 'https://api.telnyx.com/v2/ai',
+      ...opts,
+    });
+  }
 
   chat({
     chatCtx,
