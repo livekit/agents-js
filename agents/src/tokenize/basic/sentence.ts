18643163cf7c88d1a481f4601209d80d6773efa5
aoife cassidy <aoife@livekit.io>
2024-12-08 14:44:01 +0200

    source

--- agents/src/tokenize/basic/sentence.ts
+++ agents/src/tokenize/basic/sentence.ts
@@ -19,28 +19,25 @@ export const splitSentences = (text: string, minLength = 20): [string, number, n
   text = text.replaceAll('\n', ' ');
   text = text.replaceAll(prefixes, '$1<prd>');
   text = text.replaceAll(websites, '<prd>$2');
-  text = text.replaceAll(new RegExp(`${rawRegex(digits)}[.]${rawRegex(digits)}`, 'g'), '$1<prd>$2');
+  text = text.replaceAll(new RegExp(`${digits.source}[.]${digits.source}`, 'g'), '$1<prd>$2');
   text = text.replaceAll(dots, (match) => '<prd>'.repeat(match.length));
   text = text.replaceAll('Ph.D.', 'Ph<prd>D<prd>');
-  text = text.replaceAll(new RegExp(`\\s${rawRegex(alphabets)}[.] `, 'g'), ' $1<prd> ');
+  text = text.replaceAll(new RegExp(`\\s${alphabets.source}[.] `, 'g'), ' $1<prd> ');
+  text = text.replaceAll(new RegExp(`${acronyms.source} ${starters.source}`, 'g'), '$1<stop> $2');
   text = text.replaceAll(
-    new RegExp(`${rawRegex(acronyms)} ${rawRegex(starters)}`, 'g'),
-    '$1<stop> $2',
-  );
-  text = text.replaceAll(
-    new RegExp(`${rawRegex(alphabets)}[.]${rawRegex(alphabets)}[.]${rawRegex(alphabets)}[.]`, 'g'),
+    new RegExp(`${alphabets.source}[.]${alphabets.source}[.]${alphabets.source}[.]`, 'g'),
     '$1<prd>$2<prd>$3<prd>',
   );
   text = text.replaceAll(
-    new RegExp(`${rawRegex(alphabets)}[.]${rawRegex(alphabets)}[.]`, 'g'),
+    new RegExp(`${alphabets.source}[.]${alphabets.source}[.]`, 'g'),
     '$1<prd>$2<prd>',
   );
   text = text.replaceAll(
-    new RegExp(` ${rawRegex(suffixes)}[.] ${rawRegex(starters)}`, 'g'),
+    new RegExp(` ${suffixes.source}[.] ${starters.source}`, 'g'),
     '$1<stop> $2',
   );
-  text = text.replaceAll(new RegExp(` ${rawRegex(suffixes)}[.]`, 'g'), '$1<prd>');
-  text = text.replaceAll(new RegExp(` ${rawRegex(alphabets)}[.]`, 'g'), '$1<prd>');
+  text = text.replaceAll(new RegExp(` ${suffixes.source}[.]`, 'g'), '$1<prd>');
+  text = text.replaceAll(new RegExp(` ${alphabets.source}[.]`, 'g'), '$1<prd>');
   text = text.replaceAll('.”', '”.');
   text = text.replaceAll('."', '".');
   text = text.replaceAll('!"', '"!');
@@ -75,8 +72,4 @@ export const splitSentences = (text: string, minLength = 20): [string, number, n
   }
 
   return sentences;
-};
-
-const rawRegex = (r: RegExp): string => {
-  return r.toString().match(/\/(.+?)\//)![1]!;
 };
