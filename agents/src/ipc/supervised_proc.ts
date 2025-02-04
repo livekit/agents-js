7b3a7706be2fbeb3d8d9ee1a7b8a817a361eb5f8
aoife cassidy <aoife@enby.space>
2025-02-04 19:27:03 +0200

    memory

--- agents/src/ipc/supervised_proc.ts
+++ agents/src/ipc/supervised_proc.ts
@@ -25,6 +25,7 @@ export abstract class SupervisedProc {
   #runningJob?: RunningJobInfo = undefined;
   proc?: ChildProcess;
   #pingInterval?: ReturnType<typeof setInterval>;
+  #memoryWatch?: ReturnType<typeof setInterval>;
   #pongTimeout?: ReturnType<typeof setTimeout>;
   protected init = new Future();
   #join = new Future();
@@ -90,6 +91,24 @@ export abstract class SupervisedProc {
       this.#join.resolve();
     }, this.#opts.pingTimeout);
 
+    this.#memoryWatch = setInterval(() => {
+      const memoryMB = process.memoryUsage().heapUsed / (1024 * 1024);
+      if (this.#opts.memoryLimitMB > 0 && memoryMB > this.#opts.memoryLimitMB) {
+        this.#logger
+          .child({ memoryUsageMB: memoryMB, memoryLimitMB: this.#opts.memoryLimitMB })
+          .error('process exceeded memory limit, killing process');
+        this.close();
+      } else if (this.#opts.memoryWarnMB > 0 && memoryMB > this.#opts.memoryWarnMB) {
+        this.#logger
+          .child({
+            memoryUsageMB: memoryMB,
+            memoryWarnMB: this.#opts.memoryWarnMB,
+            memoryLimitMB: this.#opts.memoryLimitMB,
+          })
+          .error('process memory usage is high');
+      }
+    });
+
     const listener = (msg: IPCMessage) => {
       switch (msg.case) {
         case 'pongResponse': {
@@ -120,6 +139,7 @@ export abstract class SupervisedProc {
         .warn('job process exited unexpectedly; this likely means the error above caused a crash');
       clearTimeout(this.#pongTimeout);
       clearInterval(this.#pingInterval);
+      clearInterval(this.#memoryWatch);
       this.#join.resolve();
     });
 
