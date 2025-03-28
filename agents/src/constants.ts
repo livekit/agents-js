dacf3280b8e2326de6f3ed6d501f18210286026f
lukasIO <mail@lukasseiler.de>
2025-03-28 13:49:57 +0100

    reuse

--- agents/src/constants.ts
+++ agents/src/constants.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
 export const ATTRIBUTE_TRANSCRIPTION_TRACK_ID = 'lk.transcribed_track_id';
 export const ATTRIBUTE_TRANSCRIPTION_FINAL = 'lk.transcription_final';
 export const TOPIC_TRANSCRIPTION = 'lk.transcription';
