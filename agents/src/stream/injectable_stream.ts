708b9ae04d93dc64b3694d580c6e56ad5bd1c62c
lukasIO <mail@lukasseiler.de>
2025-05-30 13:06:43 +0200

    mutex to avoid parallel writes

--- agents/src/stream/injectable_stream.ts
+++ agents/src/stream/injectable_stream.ts
@@ -1,3 +1,4 @@
+import { Mutex } from '@livekit/mutex';
 import { mergeReadableStreams } from '@std/streams';
 import { IdentityTransform } from './identity_transform.js';
 
@@ -5,6 +6,7 @@ export class InjectableStream<T> {
   private source: ReadableStream<T>;
   private identityStream: IdentityTransform<T>;
   private mergedStream: ReadableStream<T>;
+  private injectMutex = new Mutex();
 
   constructor(source: ReadableStream<T>) {
     this.source = source;
@@ -13,16 +15,24 @@ export class InjectableStream<T> {
   }
 
   async inject(value: T) {
-    // note this will still fail for parallel writes
-    // we can acquire the writer in the constructor but this will lead to the problem with multiple sync loops blocking when trying to write
-    const writer = this.identityStream.writable.getWriter();
-    await writer.write(value);
-    await writer.close();
+    const unlock = await this.injectMutex.lock();
+    try {
+      const writer = this.identityStream.writable.getWriter();
+      await writer.write(value);
+      await writer.close();
+    } finally {
+      unlock();
+    }
   }
 
   async close() {
-    // this will not cancel the source stream but instead keep the readable open until the source finishes
-    await this.identityStream.writable.close();
+    const unlock = await this.injectMutex.lock();
+    try {
+      // this will not cancel the source stream but instead keep the readable open until the source finishes
+      await this.identityStream.writable.close();
+    } finally {
+      unlock();
+    }
   }
 
   async cancel(reason?: any) {
