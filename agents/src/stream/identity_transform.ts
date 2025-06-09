9f163741c259f7b3b01a4e021754dad28a59eec3
Shubhrakanti Ganguly <shubhrakanti.ganguly@livekit.io>
2025-06-09 15:24:28 -0700

    fix broken tests

--- agents/src/stream/identity_transform.ts
+++ agents/src/stream/identity_transform.ts
@@ -5,8 +5,14 @@ import { TransformStream } from 'node:stream/web';
 
 export class IdentityTransform<T> extends TransformStream<T, T> {
   constructor() {
-    super({
-      transform: (chunk, controller) => controller.enqueue(chunk),
-    });
+    super(
+      {
+        transform: (chunk, controller) => controller.enqueue(chunk),
+      },
+      // By default the transfor stream will buffer only one chunk at a time.
+      // In order to follow the python agents channel.py, we set set the capaciy to be effectively infinite.
+      { highWaterMark: Number.MAX_SAFE_INTEGER },
+      { highWaterMark: Number.MAX_SAFE_INTEGER },
+    );
   }
 }
