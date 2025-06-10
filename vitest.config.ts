9972ebb5fbdb921af0a3d39fe9d699519d64307c
Shubhrakanti Ganguly <shubhrakanti.ganguly@livekit.io>
2025-06-10 16:40:35 -0700

    disable test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['**/*.test.ts'],
+    // TODO (AJS-83) Re-enable once plugins are refactored with abort controllers
+    exclude: ['**/plugins/resemble/src/tts.test.ts'],
     // it is recommended to define a name when using inline configs
     name: 'nodejs',
     environment: 'node',
