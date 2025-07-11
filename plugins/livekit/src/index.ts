fdb6bcfc50bcd06a0d651962e4fa80a1b8541efd
Shubhra <shubhrakanti.ganguly@livekit.io>
2025-07-10 19:38:28 -0700

    Revert "brianyin/ajs-157-huggingface-download-utility" (#524)

--- plugins/livekit/src/index.ts
+++ plugins/livekit/src/index.ts
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2024 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
-export { downloadFileToCacheDir } from './hf_utils.js';
 export * as turnDetector from './turn_detector/index.js';
