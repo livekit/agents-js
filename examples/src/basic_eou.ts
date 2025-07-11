04fe2b46827a6b2e49d31b009fd178c90d11e0b1
Shubhra <shubhrakanti@berkeley.edu>
2025-07-10 19:31:39 -0700

    Revert "brianyin/ajs-157-huggingface-download-utility (#522)"
    This reverts commit 9d0e7536ec17b2224c3fc9324dca9487e13de532.

--- examples/src/basic_eou.ts
+++ examples/src/basic_eou.ts
@@ -1,14 +1,13 @@
 // SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
-import { type JobContext, WorkerOptions, cli, defineAgent, llm, log } from '@livekit/agents';
+import { type JobContext, WorkerOptions, cli, defineAgent, llm } from '@livekit/agents';
 import { turnDetector } from '@livekit/agents-plugin-livekit';
 import { fileURLToPath } from 'node:url';
 
 export default defineAgent({
   entry: async (ctx: JobContext) => {
     await ctx.connect();
-    const logger = log();
 
     const eouModel = new turnDetector.EnglishModel();
 
@@ -24,23 +23,14 @@ export default defineAgent({
       content: 'Hello, Brian. How can I help you today?',
     });
 
-    const chatCtxNonEndingTurn = chatCtx.copy();
-    chatCtxNonEndingTurn.addMessage({
+    chatCtx.addMessage({
       role: 'user',
-      content: 'What is weather in',
+      content: 'I am looking for a uh',
     });
 
-    const chatCtxEndingTurn = chatCtx.copy();
-    chatCtxEndingTurn.addMessage({
-      role: 'user',
-      content: 'What is weather in San Francisco?',
-    });
+    const result = await eouModel.predictEndOfTurn(chatCtx);
 
-    const resultNonEndingTurn = await eouModel.predictEndOfTurn(chatCtxNonEndingTurn);
-    const resultEndingTurn = await eouModel.predictEndOfTurn(chatCtxEndingTurn);
-
-    logger.info({ resultNonEndingTurn }, 'Non-ending turn result:');
-    logger.info({ resultEndingTurn }, 'Ending turn result:');
+    console.log(result);
   },
 });
 
