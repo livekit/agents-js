73322795a7cd1e89db3b3a6b2296a52f0607f686
lukasIO <mail@lukasseiler.de>
2025-07-29 16:59:12 +0100

    wip

---
+++ examples/src/ai_agent.ts
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2025 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+// import { mistral } from '@ai-sdk/mistral';
+import { openai } from '@ai-sdk/openai';
+import {
+  type JobContext,
+  type JobProcess,
+  WorkerOptions,
+  cli,
+  defineAgent,
+  metrics,
+  voice,
+} from '@livekit/agents';
+import * as aisdk from '@livekit/agents-plugin-aisdk';
+import * as deepgram from '@livekit/agents-plugin-deepgram';
+import * as elevenlabs from '@livekit/agents-plugin-elevenlabs';
+import * as livekit from '@livekit/agents-plugin-livekit';
+import * as silero from '@livekit/agents-plugin-silero';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'node:url';
+
+dotenv.config();
+
+export default defineAgent({
+  prewarm: async (proc: JobProcess) => {
+    proc.userData.vad = await silero.VAD.load();
+  },
+  entry: async (ctx: JobContext) => {
+    const agent = new voice.Agent({
+      instructions:
+        "You are a helpful assistant called Karla, you can hear the user's message and respond to it.",
+    });
+
+    const vad = ctx.proc.userData.vad! as silero.VAD;
+
+    const session = new voice.AgentSession({
+      vad,
+      stt: new deepgram.STT(),
+      tts: new elevenlabs.TTS(),
+      llm: new aisdk.LLM({
+        model: openai('gpt-4o-mini'),
+        prompt:
+          "You are a helpful assistant called Anton, you can hear the user's message and respond to it.",
+      }),
+
+      // to use realtime model, replace the stt, llm, tts and vad with the following
+      // llm: new openai.realtime.RealtimeModel(),
+      turnDetection: new livekit.turnDetector.EnglishModel(),
+    });
+
+    const usageCollector = new metrics.UsageCollector();
+
+    session.on(voice.AgentSessionEventTypes.MetricsCollected, (ev) => {
+      metrics.logMetrics(ev.metrics);
+      usageCollector.collect(ev.metrics);
+    });
+
+    await session.start({
+      agent,
+      room: ctx.room,
+    });
+
+    // join the room when agent is ready
+    await ctx.connect();
+
+    session.say('Hello, how can I help you today?');
+  },
+});
+
+cli.runApp(new WorkerOptions({ agent: fileURLToPath(import.meta.url) }));
