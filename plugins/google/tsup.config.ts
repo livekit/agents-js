b69d519cc28e844d0e3a925f77fdab90804e2f2e
Toubat <brian.yin@livekit.io>
2025-08-05 13:47:03 -0700

    Update tsup.config.ts

--- plugins/google/tsup.config.ts
+++ plugins/google/tsup.config.ts
@@ -1,12 +1,6 @@
-// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
 import { defineConfig } from 'tsup';
+import defaults from '../../tsup.config';
 
 export default defineConfig({
-  entry: ['src/**/*.ts'],
-  format: ['esm', 'cjs'],
-  dts: false,
-  sourcemap: true,
-  clean: true,
+  ...defaults,
 });
