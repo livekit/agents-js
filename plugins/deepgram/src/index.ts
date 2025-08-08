00758e083ad8db187e3063ce250afe1c985dacb3
Toubat <brian.yin@livekit.io>
2025-08-08 10:50:43 -0700

    add cli

--- plugins/deepgram/src/index.ts
+++ plugins/deepgram/src/index.ts
@@ -1,8 +1,7 @@
-import { Plugin } from '@livekit/agents';
-
 // SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+import { Plugin } from '@livekit/agents';
 
 export * from './stt.js';
 
--- plugins/neuphonic/src/index.ts
+++ plugins/neuphonic/src/index.ts
@@ -1,5 +1,18 @@
-// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+// SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+import { Plugin } from '@livekit/agents';
 
 export * from './tts.js';
+
+class NeurophonicPlugin extends Plugin {
+  constructor() {
+    super({
+      title: 'neuphonic',
+      version: '0.1.0',
+      package: '@livekit/agents-plugin-neuphonic',
+    });
+  }
+}
+
+Plugin.registerPlugin(new NeurophonicPlugin());
