3ebd433c253e4cd67031807ec127e43438660055
Brian Yin <brian.yin@livekit.io>
2025-08-08 10:53:14 -0700

    brianyin/ajs-210-download-files-options (#598)

--- plugins/silero/src/index.ts
+++ plugins/silero/src/index.ts
@@ -1,4 +1,18 @@
-// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+// SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+import { Plugin } from '@livekit/agents';
+
 export { VAD, VADStream } from './vad.js';
+
+class SileroPlugin extends Plugin {
+  constructor() {
+    super({
+      title: 'silero',
+      version: '0.5.6',
+      package: '@livekit/agents-plugin-silero',
+    });
+  }
+}
+
+Plugin.registerPlugin(new SileroPlugin());
