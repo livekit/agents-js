21e81a01e66c0f12d4ab6e1324c7be118ff013ae
Shubhra <shubhrakanti.ganguly@livekit.io>
2025-08-25 17:04:37 -0700

    Fix Plugin (#645)

--- plugins/neuphonic/src/tts.test.ts
+++ plugins/neuphonic/src/tts.test.ts
@@ -6,6 +6,6 @@ import { tts } from '@livekit/agents-plugins-test';
 import { describe } from 'vitest';
 import { TTS } from './tts.js';
 
-describe.skip('Neuphonic', async () => {
+describe('Neuphonic', async () => {
   await tts(new TTS(), new STT());
 });
