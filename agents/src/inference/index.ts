7a95a624e6ca65f1114bc4b0991bc559e9c69e77
Brian Yin <brian.yin@livekit.io>
2025-09-22 17:46:00 -0700

    Update agents/src/inference/index.ts
    Co-authored-by: Ben Cherry <bcherry@gmail.com>

--- agents/src/inference/index.ts
+++ agents/src/inference/index.ts
@@ -1,6 +1,11 @@
 // SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+import * as llm from './llm.js';
+import * as stt from './stt.js';
+import * as tts from './tts.js';
+
+export { stt, tts, llm };
 export { LLM, LLMStream, type LLMModels, type LLMOptions, type OpenAIModels } from './llm.js';
 export type { GatewayOptions, InferenceLLMOptions } from './llm.js';
 export { STT, type STTLanguages, type STTModels, type STTOptions } from './stt.js';
