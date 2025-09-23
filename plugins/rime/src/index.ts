1e7999bbaa86cd36bbe5396eec0e112c93fd13f8
gokuljs <gokuljs029@gmail.com>
2025-09-23 23:32:45 +0530

    index.ts file

---
+++ plugins/rime/src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './models.js';
+export { type TTSOptions, TTS, ChunkedStream } from './tts.js';
