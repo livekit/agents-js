cb74f02b39d4b59f1e6e51cb7391132e79c5c534
lukasIO <mail@lukasseiler.de>
2026-01-13 14:16:36 +0100

    Update test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
 // SPDX-FileCopyrightText: 2025 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   test: {
     include: ['**/*.test.ts'],
     // it is recommended to define a name when using inline configs
     name: 'nodejs',
     environment: 'node',
-    testTimeout: 60_000,
+    testTimeout: 5_000,
+    env: loadEnv(mode, process.cwd(), ''),
   },
-});
+}));
