a4f051f92e4dd7c4afcba96d7b0d7f0e2b103892
Brian Yin <brian.yin@livekit.io>
2026-02-17 16:17:09 -0800

    Update basic_agent_task.ts

--- examples/src/basic_agent_task.ts
+++ examples/src/basic_agent_task.ts
@@ -4,7 +4,7 @@
 import {
   type JobContext,
   type JobProcess,
-  WorkerOptions,
+  ServerOptions,
   cli,
   defineAgent,
   inference,
@@ -130,4 +130,4 @@ export default defineAgent({
   },
 });
 
-cli.runApp(new WorkerOptions({ agent: fileURLToPath(import.meta.url) }));
+cli.runApp(new ServerOptions({ agent: fileURLToPath(import.meta.url) }));
