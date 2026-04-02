6fdf2c0fa4cb47285566f81eb3fb7cbeb5bc4992
Qiong Zhou Huang <qiong@phonic.co>
2026-04-02 01:08:34 +0000

    fixes

--- agents/src/version.ts
+++ agents/src/version.ts
@@ -2,4 +2,4 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-export const version = '__PACKAGE_VERSION__';
+export const version = __PACKAGE_VERSION__;
--- examples/src/phonic_realtime_agent.ts
+++ examples/src/phonic_realtime_agent.ts
@@ -48,17 +48,17 @@ export default defineAgent({
       instructions: 'Greet the user, asking about their day.',
     });
 
-    // setTimeout(async () => {
-    //   if (session.agentState === 'initializing') return;
-    //   const agent = session.currentAgent;
-    //   const chatCtx = agent.chatCtx.copy();
-    //   chatCtx.addMessage({
-    //     role: 'system',
-    //     content:
-    //       "The user's name is Alex. He is from San Francisco and likes hiking. Use this information in your conversation.",
-    //   });
-    //   await agent.updateChatCtx(chatCtx);
-    // }, 10000);
+    setTimeout(async () => {
+      if (session.agentState === 'initializing') return;
+      const agent = session.currentAgent;
+      const chatCtx = agent.chatCtx.copy();
+      chatCtx.addMessage({
+        role: 'system',
+        content:
+          "The user's name is Alex. He is from San Francisco and likes hiking. Use this information in your conversation.",
+      });
+      await agent.updateChatCtx(chatCtx);
+    }, 10000);
   },
 });
 
