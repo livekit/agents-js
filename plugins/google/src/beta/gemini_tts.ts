84dce42cb8c3d34571b0dcfa9b4a0c93eb35cc61
Brian Yin <brian.yin@livekit.io>
2026-04-15 14:58:22 -0700

    Update gemini_tts.ts

--- plugins/google/src/beta/gemini_tts.ts
+++ plugins/google/src/beta/gemini_tts.ts
@@ -14,6 +14,7 @@ import {
 } from '@livekit/agents';
 import type { AudioFrame } from '@livekit/rtc-node';
 import type { GeminiTTSModels } from '../models.js';
+export type { GeminiTTSModels } from '../models.js';
 
 export type GeminiVoices =
   | 'Zephyr'
