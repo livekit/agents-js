0b14ea5afefb591e5ca965aa02c35dc18a231e56
Brian Yin <brian.yin@livekit.io>
2026-04-22 14:26:20 +0800

    feat(minimax): add plugin entry point

---
+++ plugins/minimax/src/index.ts
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+import { Plugin } from '@livekit/agents';
+
+export * from './models.js';
+export * from './tts.js';
+
+class MiniMaxPlugin extends Plugin {
+  constructor() {
+    super({
+      title: 'minimax',
+      version: __PACKAGE_VERSION__,
+      package: __PACKAGE_NAME__,
+    });
+  }
+}
+
+Plugin.registerPlugin(new MiniMaxPlugin());
