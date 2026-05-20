a525c4db077a4977e981b8d500065beaa2af2b58
lukasIO <mail@lukasseiler.de>
2026-05-20 13:11:35 +0200

    fix mistralai config

--- plugins/mistralai/tsdown.config.ts
+++ plugins/mistralai/tsdown.config.ts
@@ -6,9 +6,4 @@ import defaults from '../../tsdown.config.ts';
 
 export default defineConfig({
   ...defaults,
-  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
-  format: 'cjs',
-  clean: false,
-  dts: false,
-  target: false,
 });
